080c5c5f6385aa3da7cf63232a10901f885b6e9a
andy_mcfly <Andy McFly@.(none)>
2009-09-17 01:27:46 +0400

    search_combination(), added .gitignore
    search_combination - searches only pairs and triplets
    NOT TESTED, NOT FINISHED.

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <time.h>
 
 #define DECK_SIZE 52
+#define BANK_SIZE 5
+#define HAND_SIZE 2
+#define ACTIVE BANK_SIZE+HAND_SIZE
 
 
 void deck_array_generate(int deck[DECK_SIZE][2]);
@@ -70,4 +73,56 @@ void deck_array_printf(int deck[DECK_SIZE][2])
     {
         printf("\t%d\t%d\t%d\n", i, deck[i-1][0], deck[i-1][1]);
     }
+}
+
+
+
+/*
+    search_combination NOT FINISHED _ NOT TESTED
+
+    RECEIVES: array BANK and array HAND
+    EFFECT:
+        -returns the number of combination (int)
+*/
+int search_combination(int bank[BANK_SIZE][2], int hand[HAND_SIZE][2])
+{
+    int i, j, m;
+    int active[ACTIVE][2];
+    int combination=0;
+
+
+    /* We copy first BANK and then HAND to ACTIVE just to make things easier here :) */
+    for(i=0; i<ACTIVE; i++)
+    {
+        if(i<5)
+        {
+            active[i][0]=bank[i][0];
+            active[i][1]=bank[i][1];
+        }
+        else
+        {
+            active[i][0]=hand[i-BANK_SIZE][0];
+            active[i][1]=hand[i-BANK_SIZE][1];
+        }
+    }
+
+
+    /* from here we start searching the combinations*/
+    for(i=0; i<ACTIVE; i++)
+    {
+        for(j=i+1; j<ACTIVE; j++)
+        {
+            if(active[i][1]==active[j][1]&&i!=j) /* pair found NOT FINISHED!!!!!*/
+                combination=1;
+            for(m=j+1; m<ACTIVE; m++)
+            {
+                if(active[i][1]==active[j][1]==active[m][1]&&i!=j&&j!=m&&m!=i)
+                    combination=3;          /* 3 found*/
+            }
+
+        }
+    }
+
+
+    return combination;
 }
