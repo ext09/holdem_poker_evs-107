f0c1551b6471b3a13b9b29a9489a60fae598e580
andy_mcfly <Andy McFly@.(none)>
2009-09-16 22:02:39 +0400

    structure modified, rand formula added
    main.c structure modified:two separate functions
    deck_array_generate and deck_array_print.
    added randomizer formula in main.c
    	.gitignore
    	bin/

--- main.c
+++ main.c
@@ -1,35 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #define DECK_SIZE 52
+
+
+void deck_array_generate(int deck[DECK_SIZE][2]);
+void deck_array_printf(int deck[DECK_SIZE][2]);
+
 
 int main()
 {
     int deck[DECK_SIZE][2];
+    int card;
+    srand(time(NULL));
+
+    deck_array_generate(deck);
+    deck_array_printf(deck);
+
+    /*formula for randomizer*/
+    card=rand()%52;
+    printf("random card number: %d\n", card);
+
+    return 0;
+}
+
+
+/*
+    deck_array_generate
+
+    RECEIVES: integer array with # of cols - DECK_SIZE, and # of rows - 2
+    EFFECT:
+        -fills 1st col with suit number (from 1 to 4)
+        -fills 2nd col with card number in each suit (from 1 to 13)
+*/
+void deck_array_generate(int deck[DECK_SIZE][2])
+{
     int i, m=1, n=1;
-    int s; /* TEMPORARY*/
 
-    /* filling in the cards' ID in array "deck" */
-    for(i=0; i<=DECK_SIZE-1; i++)
+
+
+    for(i=0; i<DECK_SIZE; i++)
     {
-        s=i+1; /* TEMPORARY*/
-        printf("%d ", s); /* TEMPORARY*/
-
         deck[i][0]=m;
         deck[i][1]=n;
 
-        printf("%d  %d", deck[i][0], deck[i][1]); /* TEMPORARY*/
-
         if((i+1)%13==0) /* stuff to make the suit number go from 1 to 4 */
             m++;
-        if(n==13) /* stuff to make card nuber in exact suit go from 1 to 13*/
+        if(n==13) /* stuff to make card number in exact suit go from 1 to 13*/
             n=1;
         else
             n++;
-
-
-        printf("\n"); /* TEMPORARY*/
     }
+}
 
-    return 0;
+
+/*
+    deck_array_printf
+
+    RECEIVES: integer array with # of cols - DECK_SIZE, and # of rows - 2
+    EFFECT:
+        -prints out the whole array with lines' numbers
+*/
+void deck_array_printf(int deck[DECK_SIZE][2])
+{
+    int i;
+    printf("\tDeck array:\n\n");
+    for(i=1; i<=DECK_SIZE; i++)
+    {
+        printf("\t%d\t%d\t%d\n", i, deck[i-1][0], deck[i-1][1]);
+    }
 }
